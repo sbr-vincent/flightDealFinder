c13209b77130a0db057d25ada295c0c70575dfbf
sbr-vincent <vincent.g.sbr@gmail.com>
2024-03-27 16:06:30 -0700

    Initial commit

---
+++ data_manager.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+SHEETY_PRICES_ENDPOINT = os.environ.get("SHEETY_PRICES_ENDPOINT")
+
+
+# This class is responsible for talking to the Google Sheet.
+class DataManager:
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        self.destination_data = requests.get(SHEETY_PRICES_ENDPOINT).json()["prices"]
+
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+
+            print(response.text)
+
